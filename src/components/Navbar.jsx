90347e494f9c769aa1843a698f40cf427b521620
Aadarsh Jha <aadarshjha1401@gmail.com>
2023-12-18 10:40:22 +0530

    responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import GDSC_LOGO from "../assets/Logo.jpeg"
 const Navbar = () => {
   return (
     <div className="flex flex-col md:flex-row px-3 py-2 z-50 absolute top-0 left-0 right-0 justify-between text-[#202124]">
-      <div className="flex items-center">
+      <div className="flex justify-center">
         <div className="logo">
           <img src={GDSC_LOGO} className="h-[60px]" alt="gdsc_logo" />
         </div>
@@ -14,7 +14,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="flex justify-center md:justify-normal items-center">
+      <div className="flex mt-2 md:mt-0 justify-center md:justify-normal items-center">
         <div className="user flex flex-col items-end">
           <p className="text-[14px]">zhigoh69@gmail.com</p>
           <p className="text-[13px] hover:cursor-pointer hover:text-sky-500">
